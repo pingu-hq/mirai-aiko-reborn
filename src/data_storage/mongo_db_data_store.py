965b847dc7945832ae72c6a0de4bc323d3149906
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-02 23:28:25 +0800

    Feature: Added MongoDb class in asynchronous to handle concurrent request. also it will at least not block in a scenario with many request. Just basic get and post functions.

--- src/data_storage/mongo_db_data_store.py
+++ src/data_storage/mongo_db_data_store.py
@@ -1,6 +1,8 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, AsyncMongoClient
+from pymongo.asynchronous.collection import AsyncCollection
 import pymongo.errors
 from src.core.local_config import settings
+from src.core.security import SecurityAuth
 
 # 1. Connect once
 client = MongoClient(settings.mongo_db.get_secret_value())
@@ -46,3 +48,41 @@ def get_user_by_email(email: str):
 #
 # # db.drop_collection("users")
 # # coll_users.drop()
+
+# 1. Connect once
+_users_collection: AsyncCollection | None = None
+
+class MongoDbDataStore:
+    def __init__(self):
+        self.auth = SecurityAuth()
+
+    def init_db(self):
+        try:
+            self.users.create_index("email", unique=True)
+        except pymongo.errors.OperationFailure as e:
+            print(f"ℹ️ Index status: {e}")
+
+    @property
+    def users(self) -> AsyncCollection:
+        global _users_collection
+        if _users_collection is None:
+            _client = AsyncMongoClient(settings.mongo_db.get_secret_value())
+            _db = _client["mirai-aiko-database"]
+            _users_collection = _db["users"]
+        return _users_collection
+
+
+    async def create_user(self, email: str, password: str, name: str):
+        self.init_db()
+        hashed_password = await self.auth.get_hash_password(password)
+        to_user_db = {
+            "email": email,
+            "password": hashed_password,
+            "name": name
+        }
+        _insert = await self.users.insert_one(to_user_db)
+        return _insert.inserted_id
+
+    async def get_user_by_email(self, email: str):
+        self.init_db()
+        return await self.users.find_one({"email": email})
