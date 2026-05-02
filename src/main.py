f31cdebc61d8ad47945bf8538f61cd94bbdc8eb2
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-02 23:29:44 +0800

    Chore: Set up auth router in main.py for me to test

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request, status, HTTPException
+from src.routers.auth import router as auth_router
+from src.core.security import SecurityAuth
 import asyncio
 
 app = FastAPI()
+
+app.include_router(auth_router)
+
 @app.get("/hello-env-var")
 async def hello_world():
     await asyncio.sleep(1)
     from src.core.local_config import settings
     return {"message": settings.hello_world}
+
+# @app.get("/api")
+# def me(request: Request, response: Response):
+#     sa = SecurityAuth()
+#     return {"id": sa.get_cookie_id(request, response)}
+#
+# @app.get("/api/logout", status_code=status.HTTP_204_NO_CONTENT)
+# def logout_endpoint(resp: Response, req: Request):
+#     sa = SecurityAuth()
+#     if not sa.logout(req, resp):
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+#
 
 
 if __name__ == "__main__":
