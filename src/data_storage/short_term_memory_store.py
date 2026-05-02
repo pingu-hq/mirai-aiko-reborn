bfc0198cdcdad8ba67fa96ca42f6d170f1d2a730
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-02 23:30:39 +0800

    Chore: async redis is now set, but not testing have yet done for this one

--- src/data_storage/short_term_memory_store.py
+++ src/data_storage/short_term_memory_store.py
@@ -1,6 +1,6 @@
 from redis import Redis
 from typing import Any
-import redis.asyncio as redis_async
+from redis.asyncio import Redis as RedisAsync
 import json
 
 
@@ -16,3 +16,25 @@ def get_redis_by_id(_id: str):
 def set_redis(_id: str, value: Any, ttl: int = 3600):
     serialized = json.dumps(value)
     return r.set(_id, serialized, ex=ttl)
+
+class MemoryStore:
+    def __init__(self):
+        self._redis: RedisAsync | None = None
+
+    @property
+    def redis(self) -> RedisAsync:
+        if self._redis is None:
+            self._redis = RedisAsync(host='127.0.0.1', port=6379, db=0)
+        return self._redis
+
+    async def post(self, _id: str | Any, value: Any, ttl: int = 3600):
+        serialized = json.dumps(value)
+        return await self.redis.set(_id, serialized, ex=ttl)
+
+    async def get(self, _id: str | Any) -> Any:
+        serialized = await self.redis.get(_id)
+        if serialized:
+            return json.loads(serialized)
+        return None
+
+memory = MemoryStore()
