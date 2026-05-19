8d9d4bc55dc051d094eab95c5bb0d0d9e598845a
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-19 23:12:25 +0800

    Feat: Added tester router to see if it will work, this are just basic snippet to see if it will work in async. So far no problem. Would refactor after few adjustment.

---
+++ src/agentic_workflows/mirai_aiko_workflow.py
@@ -0,0 +1,28 @@
+from src.agentic_workflows.azure_agents import MiraiAikoAgent
+from src.data_storage.short_term_memory_store import MessageStore
+from fastapi import APIRouter
+from pydantic import BaseModel, Field
+
+
+router = APIRouter(
+    prefix="/api",
+    tags=["api"]
+)
+
+class SendMessage(BaseModel):
+    text: str = Field(default="", description="Text to send")
+    id: str
+
+
+@router.post("/send-message")
+async def api_send_message(message: SendMessage):
+    mem = MessageStore(user_id=message.id)
+    history = await mem.add_user_message(message.text)
+    agent = MiraiAikoAgent(input_message=history)
+    response = await agent.aexecute()
+    await mem.add_assistant_message(response)
+    return SendMessage(text=response, id=message.id)
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Response, Request, status, HTTPException
 from src.routers.auth import router as auth_router
+from src.agentic_workflows.mirai_aiko_workflow import router as test_router
 from src.core.security import SecurityAuth
 import asyncio
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(test_router)
 
 @app.get("/hello-env-var")
 async def hello_world():
