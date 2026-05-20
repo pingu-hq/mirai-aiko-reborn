e6079d8987b15d4d82ecca26ee4926ca2cbb896a
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-21 01:14:08 +0800

    Refactor: Migrate these functions to class as GraphEngine

--- src/agentic_workflows/mirai_aiko_workflow.py
+++ src/agentic_workflows/mirai_aiko_workflow.py
@@ -1,28 +1,11 @@
 from src.agentic_workflows.azure_agents import MiraiAikoAgent
 from src.data_storage.short_term_memory_store import MessageStore
-from fastapi import APIRouter
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from langgraph.graph import StateGraph, END
 
 
-router = APIRouter(
-    prefix="/api",
-    tags=["api"]
-)
-
-class SendMessage(BaseModel):
-    text: str = Field(default="", description="Text to send")
-    id: str
 
 
-@router.post("/send-message")
-async def api_send_message(message: SendMessage):
-    mem = MessageStore(user_id=message.id)
-    history = await mem.add_user_message(message.text)
-    agent = MiraiAikoAgent(input_message=history)
-    response = await agent.aexecute()
-    await mem.add_assistant_message(response)
-    return SendMessage(text=response, id=message.id)
 
 
 class StateChat(BaseModel):
@@ -32,35 +15,45 @@ class StateChat(BaseModel):
     chat_history_holder: list[dict] = []
 
 
-async def get_history(state: StateChat):
-    mem = MessageStore(user_id=state.id)
-    state.chat_history_holder = await mem.add_user_message(input_message=state.input_message)
-    return state
 
-async def mirai_aiko_agent(state: StateChat):
-    history = state.chat_history_holder
-    agent = MiraiAikoAgent(input_message=history)
-    state.output_message = await agent.aexecute()
-    return state
+class GraphEngine:
+    def __init__(self):
+        self._workflow = StateGraph(StateChat)
 
-async def update_memory(state: StateChat):
-    mem = MessageStore(user_id=state.id)
-    final_chat_history = await mem.add_assistant_message(input_message=state.output_message)
-    state.chat_history_holder = final_chat_history
-    return state
+    @staticmethod
+    async def get_history(state: StateChat) -> dict:
+        mem = MessageStore(user_id=state.id)
+        history = await mem.add_user_message(input_message=state.input_message)
+        return { 'chat_history_holder': history}
+
+    @staticmethod
+    async def mirai_aiko_agent(state: StateChat) -> dict:
+        history = state.chat_history_holder
+        agent = MiraiAikoAgent(input_message=history)
+        output_message = await agent.aexecute()
+        return {'output_message': output_message}
+
+    @staticmethod
+    async def update_memory(state: StateChat) -> dict:
+        mem = MessageStore(user_id=state.id)
+        final_chat_history = await mem.add_assistant_message(input_message=state.output_message)
+        return {'chat_history_holder': final_chat_history}
+
+    def workflow(self):
+        w = self._workflow
+        w.add_node("get_history", self.get_history)
+        w.add_node("mirai_aiko_agent", self.mirai_aiko_agent)
+        w.add_node("update_memory", self.update_memory)
+
+        w.set_entry_point("get_history")
+        w.add_edge("get_history", "mirai_aiko_agent")
+        w.add_edge("mirai_aiko_agent", "update_memory")
+        w.add_edge("update_memory", END)
+
+        self._workflow = w
+        return self._workflow
 
 
-workflow = StateGraph(StateChat)
-workflow.add_node("first_node", get_history)
-workflow.add_node("second_node", mirai_aiko_agent)
-workflow.add_node("third_node", update_memory)
-
-workflow.set_entry_point("first_node")
-workflow.add_edge("first_node", "second_node")
-workflow.add_edge("second_node", "third_node")
-workflow.add_edge("third_node", END)
-
-app = workflow.compile()
 
 
 
