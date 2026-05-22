a47d8aa03715cc65d9a27a6b4ceb66788a300763
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-23 02:02:01 +0800

    Chore: Migrate class to another file to ensure we will not mix logics with others.

---
+++ src/data_storage/all_vector_database_connections/character_vector.py
@@ -0,0 +1,43 @@
+from fastapi import  Request
+from llama_index.core import VectorStoreIndex
+from llama_index.core.schema import NodeWithScore
+
+
+
+
+
+
+class CharacterVectorStore:
+    def __init__(self, request: Request):
+        self.request = request
+
+    @property
+    def index(self) -> VectorStoreIndex:
+        return self.request.app.state.vector_store_index
+
+    @staticmethod
+    def _memory_organizer(raw_memories: list[NodeWithScore]):
+        cleansed_parts = []
+
+        for i, mem in enumerate(raw_memories):
+            score = f"{mem.score:.3f}"
+            description = mem.metadata.get("description", "No description available.")
+            dialogue = mem.metadata.get("dialogue", "")
+
+            template = (
+                f"<memory_node index='{i + 1}'>\n"
+                f"  <confidence>{score}</confidence>\n"
+                f"  <trait>{description}</trait>\n"
+                f"  <behavioral_sample>{dialogue}</behavioral_sample>\n"
+                f"</memory_node>"
+            )
+            cleansed_parts.append(template)
+
+        final_context = "<retrieved_memories>\n" + "\n".join(cleansed_parts) + "\n</retrieved_memories>"
+        return final_context
+
+    async def search_memories_by_query(self, query: str):
+        memories = self.index.as_retriever(similarity_top_k=5)
+        raw_memories = await memories.aretrieve(query)
+        cleaned_memories = self._memory_organizer(raw_memories=raw_memories)
+        return cleaned_memories
