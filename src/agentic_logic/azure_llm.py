b07fe7ac154f328b6b5b2181f3d6bb95cd059909
Nana Mein <alfeoephraimquintay@gmail.com>
2026-05-24 15:59:10 +0800

    Feature: (SOLID Code - SRP and OCP) Added base class for LLM, this will now be open for extension and close for modification type of class. Different LLM Model can be used into their own different purposes

--- src/agentic_logic/azure_llm.py
+++ src/agentic_logic/azure_llm.py
@@ -1,8 +1,12 @@
+from typing import Any
+from fastapi import HTTPException, status
 from openai import AsyncOpenAI, OpenAI
+from pydantic import BaseModel, ConfigDict
 from core.local_config import settings
 from fastapi import Request
 from dotenv import load_dotenv
 from pathlib import Path
+from abc import ABC, abstractmethod
 import os
 
 load_dotenv(Path(__file__).parent.parent.parent.resolve(True) / "secrets" / "agent_configs.txt")
@@ -20,18 +24,77 @@ def load_azure_client():
 
 
 def lifespan_context_azure_llm():
-    return load_azure_client()[1]
+    return load_azure_client()
 
-def basic_llm_run(request: Request, content: str):
-    client = request.app.state.azure_llm
-    completion = client.chat.completions.create(
-        model=os.environ["LLM_TYPE_A"],
-        messages=[
-            {
-                "role": "user",
-                "content": content,
-            }
-        ],
-    )
 
-    return completion.choices[0].message.content
+
+class LLMBase(ABC, BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    request: Request | None
+    content: str | list[dict[str, str]]
+    model_type: str
+
+
+    @property
+    def _llm(self):
+        if self.request is None:
+            return lifespan_context_azure_llm()[1]
+        return self.request.app.state.azure_llm[1]
+
+    @property
+    def _llm_async(self):
+        if self.request is None:
+            return lifespan_context_azure_llm()[0]
+        return self.request.app.state.azure_llm[0]
+
+    @property
+    def _message_content(self):
+        return self._internal_content_to_message(self.content)
+
+    @property
+    def _model(self):
+        try:
+            return os.environ[self.model_type]
+        except KeyError:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Model not found")
+
+
+    @staticmethod
+    def _internal_content_to_message(content: str | list[dict]) -> list[Any | dict[str, Any | str]]:
+        if isinstance(content, str):
+            return [{"role": "user", "content": content}]
+
+        allowed_roles = ["user", "system"]
+        if isinstance(content, list):
+            if content[0].get("role") in allowed_roles:
+                return content
+
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Content not allowed")
+
+    def _internal_message_to_params(self, **kwargs):
+        return {
+            **kwargs,
+            "model":self._model,
+            "messages" : self._message_content,
+        }
+
+    def chat_completion_sync(self, **extra):
+        params = self._internal_message_to_params(**extra)
+        return self._llm.chat.completions.create(**params)
+
+    async def chat_completion_async(self, **extra):
+        params = self._internal_message_to_params(**extra)
+        return await self._llm_async.chat.completions.create(**params)
+
+    @abstractmethod
+    def parameter_requirements(self):
+        pass
+
+
+    @abstractmethod
+    async def run_async(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
