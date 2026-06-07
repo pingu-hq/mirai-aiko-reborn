1dea4ffe75bfb7a87d6cbd74154824fd6c1ab3e1
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-08 00:48:00 +0800

    Refactor: Change the auth, removing the old, and adding the new one with jwt http only cookie. From the early developement, this is originally authorized bearer in headers, but with research i found it has lots of vulnerabilities so i go to httponly cookie. But with current messed up code, i decide to apply a bit of solid code, and now i make this auth router somewhat cleaner than before. This is not final as i would still fix models, input and output formats using pydantic.

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,74 +1,128 @@
-from fastapi import APIRouter, status, HTTPException, Response, Request
+from fastapi import APIRouter, Response, Request, status, HTTPException, Depends
 from pydantic import BaseModel
-from src.core.security import SecurityAuth
-from src.data_storage.mongo_db_data_store import MongoDbDataStore
-import asyncio
+from services.auth.web_auth_service import (
+    JwtTokenService,
+    HttpCookieManagerService,
+    JwtAndCookieHandlerService
+)
+from services.auth.user_auth_services import UserAuthenticationService
+from services.auth.hash_password_service import AuthPasswordService
+from repositories.no_sql_database.mongo_db_repository import UsersCollectionRepository
+from repositories.in_memory_database.redis_repository import JtiCacheRepository
 
+
+def get_jti_repo() -> JtiCacheRepository:
+    return JtiCacheRepository()
+
+
+def get_jwt_service(jti_repo: JtiCacheRepository = Depends(get_jti_repo)) -> JwtTokenService:
+    return JwtTokenService(jti_repo)
+
+
+def get_pass_service() -> AuthPasswordService:
+    return AuthPasswordService()
+
+
+def get_user_mongo_db() -> UsersCollectionRepository:
+    return UsersCollectionRepository()
+
+
+def get_user_auth_service(
+        pass_service: AuthPasswordService = Depends(get_pass_service),
+        user_mongo_db: UsersCollectionRepository = Depends(get_user_mongo_db)
+) -> UserAuthenticationService:
+    return UserAuthenticationService(
+        pass_service, user_mongo_db
+    )
+
+
+def get_jwt_and_cookie_handler_service(
+        request: Request,
+        response: Response,
+):
+    return JwtAndCookieHandlerService(request, response)
+
+
+def get_http_cookie_service(
+        handler_service: JwtAndCookieHandlerService = Depends(get_jwt_and_cookie_handler_service),
+        jwt_service: JwtTokenService = Depends(get_jwt_service),
+):
+    return HttpCookieManagerService(
+        jwt_service=jwt_service, auth_handler_service=handler_service
+    )
 
 
 router = APIRouter(
-    prefix="/auth",
-    tags=["auth"],
+    prefix="/api/auth",
+    tags=["auth"]
 )
 
-class Login(BaseModel):
+
+class User(BaseModel):
     email: str
     password: str
 
-class SignUp(Login):
-    name: str
+
+class UserRead(BaseModel):
+    external_id: str
+
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-async def login_endpoint(login: Login, resp: Response, request: Request):
-    _error = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized!")
-    sa = SecurityAuth()
-    if sa.check_current_user_logged_in(request=request):
-        # print("ERROR IN EXISTING USER IN COOKIE")
-        raise _error
-
+async def login_endpoint_v1(
+        user: User,
+        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_service),
+        user_auth_service: UserAuthenticationService = Depends(get_user_auth_service),
+):
     try:
-        mongo_db = MongoDbDataStore()
-        user_info_id, user_info_hash_pass = await mongo_db.get_id_and_password(login.email)
-        if user_info_id is None or user_info_hash_pass is None:
-            raise _error
-
-        validity = await sa.login_with_cookie(
-            user=user_info_id,
-            password=login.password,
-            hashed_password=user_info_hash_pass,
-            resp=resp
+        user_account = await user_auth_service.login_user(
+            email=user.email,
+            password=user.password
         )
-        if not validity:
-            # print("ERROR IN VALIDITY OF LOGIN USER")
-            raise _error
-        return {"status":"ok"}
+        if not http_cookie.setting_http_cookie(sub=user_account):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
-    except Exception as ex:
-        print(str(ex))
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-@router.post("/sign-up", status_code=status.HTTP_201_CREATED)
-async def sign_up_endpoint(sign_user: SignUp):
-    try:
-        mongo_db = MongoDbDataStore()
-        created_user = await mongo_db.create_user(
-            email=sign_user.email,
-            name=sign_user.name,
-            password=sign_user.password
-        )
-        if not created_user:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT)
-        return {"status":"ok"}
+        return UserRead(external_id=user_account)
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=str(e))
+        print(str(e))
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
-@router.get("/me", status_code=status.HTTP_202_ACCEPTED)
-def check_cookie_good(resp: Response, req: Request):
-    sa = SecurityAuth()
-    _id = sa.get_cookie_id(req, resp)
-    return {"unique_identifier":_id}
 
-@router.get("/logout", status_code=status.HTTP_204_NO_CONTENT)
-def logout_endpoint(request: Request):
-    sa = SecurityAuth()
-    sa.revocation_of_jti(request=request)
+@router.post("/signup", status_code=status.HTTP_201_CREATED)
+async def signup_endpoint_v1(
+        user: User,
+        user_auth_service: UserAuthenticationService = Depends(get_user_auth_service)
+):
+    try:
+        user_account = await user_auth_service.sign_up_user(
+            email=user.email,
+            password=user.password
+        )
+        return UserRead(external_id=user_account)
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+@router.get("/me")
+async def check_user_v1(
+        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_service),
+
+):
+    try:
+        external_id, jti = http_cookie.getting_id_from_http_cookie_with_jti()
+        if not external_id:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        return {
+            "external_id": external_id,
+            "jti_key": jti,
+        }
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+@router.delete("/logout", status_code=status.HTTP_204_NO_CONTENT)
+async def logout_endpoint_v1(
+        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_service),
+):
+    http_cookie.deleting_id_from_http_cookie()
