6ae008e593052d1835675ddfccc5eaa54bdd1d55
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-08 00:25:20 +0800

    Feature: Added base class, client for mongodb and some ready made collections that i will need later on. For now this is foundational structure for mongo db as repository.

---
+++ src/repositories/no_sql_database/mongo_db_repository.py
@@ -0,0 +1,42 @@
+from pymongo import AsyncMongoClient
+from pymongo.asynchronous.database import AsyncDatabase, AsyncCollection
+from src.core.local_config import settings
+
+
+
+def get_db() -> AsyncDatabase:
+    if not hasattr(get_db, "database"):
+        _database_name = "database-name-test"
+        _mongo_client = AsyncMongoClient(settings.mongo_db.get_secret_value())
+        get_db.database = _mongo_client[_database_name]
+
+    return get_db.database
+
+
+class AsyncMongoDatabase:
+
+    @property
+    def db(self) -> AsyncDatabase:
+        return get_db()
+
+    def get_collection_by_name(self, collection_name: str) -> AsyncCollection:
+        return self.db[collection_name]
+
+
+class UsersCollectionRepository:
+    def __init__(self):
+        self.collection_name = "users"
+
+    @property
+    def users(self) -> AsyncCollection:
+        _db = AsyncMongoDatabase()
+        return _db.get_collection_by_name(self.collection_name)
+
+class ConversationsCollectionRepository:
+    def __init__(self):
+        self.collection_name = "conversations"
+
+    @property
+    def conversations(self):
+        _db = AsyncMongoDatabase()
+        return _db.get_collection_by_name(self.collection_name)
