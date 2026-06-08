fb277d3d56cc49a049339bbe3cab966ffdae1db2
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-08 16:21:07 +0800

    Feature: Added milvus vector store index in state.py

--- app/core/state.py
+++ app/core/state.py
@@ -1,5 +1,6 @@
 from redis import Redis
 from pymongo.asynchronous.database import AsyncDatabase
+from llama_index.core import VectorStoreIndex
 
 
 
@@ -8,5 +9,7 @@ from pymongo.asynchronous.database import AsyncDatabase
 class AppState:
     redis_jwt_client: Redis | None = None
     mongo_db_client: AsyncDatabase | None = None
+    milvus_character_index: VectorStoreIndex | None = None
+    milvus_message_index: VectorStoreIndex | None = None
 
 app_state = AppState()
