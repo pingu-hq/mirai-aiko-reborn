6b35a78bba816b7329bd4ed7182c2e688c5abc3e
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-08 16:21:39 +0800

    Feature: Added milvus vector store index clients in lifespan.py

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -2,6 +2,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from pymongo.asynchronous.database import AsyncDatabase
 from pymongo import AsyncMongoClient
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.milvus import MilvusVectorStore
+from llama_index.embeddings.cohere import CohereEmbedding
+from datetime import timedelta
 
 
 from app.core.local_config import settings
@@ -27,12 +31,78 @@ def get_mongo_db_main_client() -> AsyncDatabase:
 
 
 
+class MainMilvusClients:
+    def __init__(self):
+        self._embed_model = None
+        self.api_key = settings.cohere_api_key
+        self.uri = settings.milvus_uri
+        self.token = settings.milvus_token
+        self.ttl = int(timedelta(days=7).total_seconds())
+
+    @property
+    def embed_model(self):
+        if self._embed_model is None:
+            self._embed_model = CohereEmbedding(
+                model_name="embed-multilingual-v3.0",
+                api_key=self.api_key.get_secret_value()
+            )
+        return self._embed_model
+
+    @property
+    def character_knowledge_index(self):
+        _vector_store = MilvusVectorStore(
+            collection_name="character_knowledge_base",
+            **self._vector_config_params()
+        )
+        return self._indexing(vector_store=_vector_store)
+
+    @property
+    def message_store_index(self):
+        _vector_store = MilvusVectorStore(
+            collection_name="temporary_message_collection",
+            collection_properties={"collection.ttl.seconds": self.ttl},
+            **self._vector_config_params()
+        )
+        return self._indexing(vector_store=_vector_store)
+
+    def _indexing(self, vector_store: MilvusVectorStore):
+        return VectorStoreIndex.from_vector_store(
+            vector_store=vector_store,
+            embed_model=self.embed_model,
+            use_async=True
+        )
+
+    def _vector_config_params(self):
+        return {
+            "uri" : self.uri.get_secret_value(),
+            "token" : self.token.get_secret_value(),
+            "overwrite" : False,
+            "dim" : 1024,
+            "embedding_field" : "embeddings",
+            "search_config" : {"nprobe": 60},
+            "similarity_metric" :  "COSINE",
+            "consistency_level" : "Session",
+        }
+
+def get_character_knowledge_index():
+    milvus_object = MainMilvusClients()
+    return milvus_object.character_knowledge_index
+
+def get_message_store_index():
+    milvus_object = MainMilvusClients()
+    return milvus_object.message_store_index
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app_state.redis_jwt_client = get_jwt_redis_client()
     app_state.mongo_db_client = get_mongo_db_main_client()
+    app_state.milvus_character_index = get_character_knowledge_index()
+    app_state.milvus_message_index = get_message_store_index()
 
     yield
 
     app_state.redis_jwt_client.close()
+    app_state.milvus_message_index = None
+    app_state.milvus_character_index = None
+    app_state.mongo_db_client = None
