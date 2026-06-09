03c7eff4747ff9f5402d22c1b143ed9b8f70295f
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-09 12:05:16 +0800

    Feature: Added new attributes, well be doing azure, cohere, and httpx clients.

--- app/core/state.py
+++ app/core/state.py
@@ -1,6 +1,9 @@
 from redis import Redis
 from pymongo.asynchronous.mongo_client import AsyncMongoClient
 from llama_index.vector_stores.milvus import MilvusVectorStore
+from llama_index.embeddings.cohere import CohereEmbedding
+from openai import OpenAI
+from httpx import AsyncClient, Client
 
 
 
@@ -11,5 +14,9 @@ class AppState:
     mongo_db_client: AsyncMongoClient | None = None
     milvus_character_vector: MilvusVectorStore | None = None
     milvus_message_vector: MilvusVectorStore | None = None
+    azure_client: OpenAI | None = None
+    cohere_embed_model: CohereEmbedding | None = None
+    httpx_client: Client | None = None
+    httpx_async_client: AsyncClient | None = None
 
 app_state = AppState()
