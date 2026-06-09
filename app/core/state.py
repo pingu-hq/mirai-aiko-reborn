b6e9566e281acc829b5925293cf7b94ea0af1206
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-09 12:25:35 +0800

    Chore: Made dataclass, so that if we accidentally add attributes, dataclass can stop that.

--- app/core/state.py
+++ app/core/state.py
@@ -4,11 +4,13 @@ from llama_index.vector_stores.milvus import MilvusVectorStore
 from llama_index.embeddings.cohere import CohereEmbedding
 from openai import OpenAI
 from httpx import AsyncClient, Client
+from dataclasses import dataclass
 
 
 
 
 
+@dataclass(slots=True)
 class AppState:
     redis_jwt_client: Redis | None = None
     mongo_db_client: AsyncMongoClient | None = None
