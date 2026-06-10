685501ee654b756094e6d12184aa682cceac82ab
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-10 23:55:31 +0800

    Feature: Added dependencies directory. In here we services and repository will be abstracted before in sending it to router to minimize noise and verbosity. ALso added auth.py as initial file. In here, we dont need to explicitly add and instance classes. With this, we can follow DRY principle.

---
+++ app/dependencies/auth.py
@@ -0,0 +1,55 @@
+from app.repositories.in_memory_database.redis_repository import JtiCacheRepository
+from app.repositories.no_sql_database.mongo_db_repository import UsersCollectionRepository
+from app.services.auth.hash_password_service import AuthPasswordService
+from app.services.auth.web_auth_service import JwtTokenService, JwtAndCookieHandlerService, HttpCookieManagerService
+from app.services.auth.user_auth_services import UserAuthenticationService
+from fastapi import Depends, Request, Response
+
+
+
+def get_redis_repository() -> JtiCacheRepository:
+    return JtiCacheRepository()
+
+
+def get_jwt_token_service(
+        jti_repository: JtiCacheRepository = Depends(get_redis_repository)
+) -> JwtTokenService:
+    return JwtTokenService(jti_repository=jti_repository)
+
+
+def get_jwt_and_cookie_handler_service(
+        request: Request,
+        response: Response,
+) -> JwtAndCookieHandlerService:
+    return JwtAndCookieHandlerService(
+        request=request,
+        response=response,
+    )
+
+
+def get_http_cookie_manager_service(
+        jwt_service: JwtTokenService = Depends(get_jwt_token_service),
+        auth_handler_service: JwtAndCookieHandlerService = Depends(get_jwt_and_cookie_handler_service)
+) -> HttpCookieManagerService:
+    return HttpCookieManagerService(
+        jwt_service=jwt_service,
+        auth_handler_service=auth_handler_service
+    )
+
+
+def get_user_collection_repository() -> UsersCollectionRepository:
+    return UsersCollectionRepository()
+
+
+def get_auth_password_service() -> AuthPasswordService:
+    return AuthPasswordService()
+
+
+def get_user_authentication_service(
+        auth_pass_service: AuthPasswordService = Depends(get_auth_password_service),
+        mongo_db: UsersCollectionRepository = Depends(get_user_collection_repository)
+) -> UserAuthenticationService:
+    return UserAuthenticationService(
+        auth_pass_service=auth_pass_service,
+        mongo_db=mongo_db
+    )
