6d50e73ce10fe3f0ea2d7a9bff5472a005e14703
Nana Mein <alfeoephraimquintay@gmail.com>
2026-06-11 00:00:47 +0800

    Refactor: Refactor auth.py, now we remove these boilerplate code that could be duplicated in other auth as well. So now that we have /dependecies as directory folder, in there, we can separate and reuse http service and auth service to other endpoints and routers if you ask me. This also ensures that can can add something on top of the other services. in dependencies if we have different logics and so on.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,55 +1,11 @@
-from fastapi import APIRouter, Response, Request, status, HTTPException, Depends
+from fastapi import APIRouter, status, HTTPException, Depends
 from pydantic import BaseModel
-from app.services.auth.web_auth_service import (
-    JwtTokenService,
-    HttpCookieManagerService,
-    JwtAndCookieHandlerService
-)
+from app.services.auth.web_auth_service import HttpCookieManagerService
 from app.services.auth.user_auth_services import UserAuthenticationService
-from app.services.auth.hash_password_service import AuthPasswordService
-from app.repositories.no_sql_database.mongo_db_repository import UsersCollectionRepository
-from app.repositories.in_memory_database.redis_repository import JtiCacheRepository
+from app.dependencies.auth import get_http_cookie_manager_service, get_user_authentication_service
 
 
-def get_jti_repo() -> JtiCacheRepository:
-    return JtiCacheRepository()
 
-
-def get_jwt_service(jti_repo: JtiCacheRepository = Depends(get_jti_repo)) -> JwtTokenService:
-    return JwtTokenService(jti_repo)
-
-
-def get_pass_service() -> AuthPasswordService:
-    return AuthPasswordService()
-
-
-def get_user_mongo_db() -> UsersCollectionRepository:
-    return UsersCollectionRepository()
-
-
-def get_user_auth_service(
-        pass_service: AuthPasswordService = Depends(get_pass_service),
-        user_mongo_db: UsersCollectionRepository = Depends(get_user_mongo_db)
-) -> UserAuthenticationService:
-    return UserAuthenticationService(
-        pass_service, user_mongo_db
-    )
-
-
-def get_jwt_and_cookie_handler_service(
-        request: Request,
-        response: Response,
-):
-    return JwtAndCookieHandlerService(request, response)
-
-
-def get_http_cookie_service(
-        handler_service: JwtAndCookieHandlerService = Depends(get_jwt_and_cookie_handler_service),
-        jwt_service: JwtTokenService = Depends(get_jwt_service),
-):
-    return HttpCookieManagerService(
-        jwt_service=jwt_service, auth_handler_service=handler_service
-    )
 
 
 router = APIRouter(
@@ -70,8 +26,8 @@ class UserRead(BaseModel):
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_endpoint_v1(
         user: User,
-        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_service),
-        user_auth_service: UserAuthenticationService = Depends(get_user_auth_service),
+        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
+        user_auth_service: UserAuthenticationService = Depends(get_user_authentication_service),
 ):
     try:
         user_account = await user_auth_service.login_user(
@@ -90,7 +46,7 @@ async def login_endpoint_v1(
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def signup_endpoint_v1(
         user: User,
-        user_auth_service: UserAuthenticationService = Depends(get_user_auth_service)
+        user_auth_service: UserAuthenticationService = Depends(get_http_cookie_manager_service)
 ):
     try:
         user_account = await user_auth_service.sign_up_user(
@@ -105,7 +61,7 @@ async def signup_endpoint_v1(
 
 @router.get("/me")
 async def check_user_v1(
-        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_service),
+        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
 
 ):
     try:
@@ -123,6 +79,6 @@ async def check_user_v1(
 
 @router.delete("/logout", status_code=status.HTTP_204_NO_CONTENT)
 async def logout_endpoint_v1(
-        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_service),
+        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
 ):
     http_cookie.deleting_id_from_http_cookie()
