e55141c666ba13f0b8e3d3b641bf332f6cc65fba
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-07 11:07:29 +0800

    Refactor: With the new login service, i simple replace the old codes and use this one, a cleaner and modular way of doing things. Now login service for only login endpoint. Others wont depend on it.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,10 @@
 from fastapi import APIRouter, status, HTTPException, Depends
-from pydantic import BaseModel
 from app.services.auth.web_auth_service import HttpCookieManagerService
 from app.services.auth.user_auth_services import UserAuthenticationService
-from app.dependencies.auth import get_http_cookie_manager_service, get_user_authentication_service
+from app.dependencies.auth import get_http_cookie_manager_service, get_auth_user_service
 from app.core.logger import app_logger
-
-
-
-
+from app.services.auth.user_auth_services import AuthUserLoginService
+from app.schemas.users import *
 
 router = APIRouter(
     prefix="/api/auth",
@@ -15,40 +12,28 @@ router = APIRouter(
 )
 
 
-class User(BaseModel):
-    email: str
-    password: str
-
-
-class UserRead(BaseModel):
-    external_id: str
-
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_endpoint_v1(
-        user: User,
+        user: UserLoginRequestRequest,
         http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
-        user_auth_service: UserAuthenticationService = Depends(get_user_authentication_service),
+        login_service: AuthUserLoginService = Depends(get_auth_user_service)
 ):
     try:
-        user_account = await user_auth_service.login_user(
-            email=user.email,
-            password=user.password
-        )
-        if not http_cookie.setting_http_cookie(sub=user_account):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        await login_service.insert_email(email=user.email)
+        if not await login_service.verify_password(password=user.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized Error")
+
+        external_id = login_service.get_external_id()
+        http_cookie.setting_http_cookie(sub=external_id)
 
         app_logger.info(f"The User ({user.email}) has successfully logged in")
-        return UserRead(external_id=user_account)
+        return UserLoginResponse(email=user.email)
     except Exception as e:
         app_logger.error(f"User login error: {e}")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized Error")
 
 
-@router.post("/signup", status_code=status.HTTP_201_CREATED)
-async def signup_endpoint_v1(
-        user: User,
-        user_auth_service: UserAuthenticationService = Depends(get_http_cookie_manager_service)
 ):
     try:
         user_account = await user_auth_service.sign_up_user(
