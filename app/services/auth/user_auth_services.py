12e046ad6a33a4f59e3e769a5bf36de057eca12d
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-07 11:00:09 +0800

    Feature: Create a separate service for login, this ensures that its not bloated and this is much flexible. Implementing solid code here.

--- app/services/auth/user_auth_services.py
+++ app/services/auth/user_auth_services.py
@@ -69,3 +69,62 @@ class UserAuthenticationService:
         except Exception as e:
             app_logger.error(f"Login user {email} failed. Error: {e}")
             return None
+
+
+class AuthUserLoginService:
+    _external_id = "external_id"
+    _password = "password"
+    _date_created = "date_created"
+
+    def __init__(
+            self,
+            auth_pass_service: AuthPasswordService,
+            mongo_db: UsersCollectionRepository,
+    ):
+        self.mongo_db = mongo_db
+        self.auth_pass_service = auth_pass_service
+        self.document_state = None
+        self.email_as_key = None
+
+    async def insert_email(self, email):
+        try:
+            self.email_as_key = email
+            self.document_state = await self.mongo_db.users.find_one({"email": email})
+            if self.document_state is None:
+                return False
+            return True
+        except Exception as e:
+            self.document_state = None
+            self.email_as_key = None
+            return False
+
+    @staticmethod
+    def state_error():
+        raise Exception("State is None. Please insert email through insert_email function")
+
+    def internal_get_state(self, key):
+        if self.document_state is None:
+            raise self.state_error()
+        value = self.document_state.get(key)
+        if not value:
+            return None
+        return value
+
+
+    def get_external_id(self):
+        return self.internal_get_state(self._external_id)
+
+    def get_password(self):
+        return self.internal_get_state(self._password)
+
+    def get_date_created(self):
+        return self.internal_get_state(self._date_created)
+
+    async def verify_password(self, password):
+        current_hashed_password = self.document_state.get("password")
+        is_valid = await self.auth_pass_service.verify_hash_password(
+            password=password, hash_password=current_hashed_password
+        )
+        if not is_valid:
+            return False
+        return True
