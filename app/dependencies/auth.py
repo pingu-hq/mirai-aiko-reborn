9c313e5da73622f7dadaca218035f11f7f38021e
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-07 11:55:07 +0800

    Feat: Added new sign up service for dependencies

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -2,7 +2,7 @@ from app.repositories.in_memory_database.redis_repository import JtiCacheReposit
 from app.repositories.no_sql_database.mongo_db_repository import UsersCollectionRepository
 from app.services.auth.hash_password_service import AuthPasswordService
 from app.services.auth.web_auth_service import JwtTokenService, JwtAndCookieHandlerService, HttpCookieManagerService
-from app.services.auth.user_auth_services import UserAuthenticationService, AuthUserLoginService
+from app.services.auth.user_auth_services import UserAuthenticationService, AuthUserLoginService, AuthUserRegisterService
 from fastapi import Depends, Request, Response
 
 
@@ -59,6 +59,15 @@ def get_user_login_service(
         mongo_db: UsersCollectionRepository = Depends(get_user_collection_repository)
 ):
     return AuthUserLoginService(
+        auth_pass_service=auth_pass_service,
+        mongo_db=mongo_db
+    )
+
+def get_user_create_service(
+        auth_pass_service: AuthPasswordService = Depends(get_auth_password_service),
+        mongo_db: UsersCollectionRepository = Depends(get_user_collection_repository)
+):
+    return AuthUserRegisterService(
         auth_pass_service=auth_pass_service,
         mongo_db=mongo_db
     )
