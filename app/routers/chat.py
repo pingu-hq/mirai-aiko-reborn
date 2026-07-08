d6aecc216aad5ea7a8a9589093fa9cfacd39c062
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-08 20:00:26 +0800

    Refactor: chat router is now using simplified get user id logic from dependencies.

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.services.agents.sample_agent_service import AgentService
-from app.services.auth.web_auth_service import  HttpCookieManagerService
-from app.dependencies.auth import get_http_cookie_manager_service
+from app.dependencies.auth import get_user_id_from_cookie
 from app.dependencies.chat import get_agent_service
 
 
@@ -18,19 +17,17 @@ class Chat(BaseModel):
 @router.post("/send", status_code=status.HTTP_200_OK)
 async def send_messages(
         chat: Chat,
-        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
+        user_id: str = Depends(get_user_id_from_cookie),
         agent: AgentService = Depends(get_agent_service)
 ):
     try:
-        user_id = http_cookie.getting_id_from_http_cookie()
-        if user_id is None:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not found")
-
         assistant_response = await agent.full_message_completion(
             user_id=user_id,
             content=chat.message
         )
         return Chat(message=assistant_response)
+    except HTTPException:
+        raise
     except Exception as e:
         print(f"Error endpoint /send : {e}")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
