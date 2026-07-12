ee4fa313b2db89f25223f4a76cca2827e84dc444
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-12 13:44:21 +0800

    Chore: Extending exception function by doing Solid code, srp and ocp. A simple concept, because custom check runtime might be more useful in different situation.

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -2,7 +2,11 @@ from typing import TypeVar, cast
 
 T = TypeVar("T")
 
-def check_property_runtime(name:str, singleton_variable: T | None) -> T:
+def custom_check_runtime(singleton_variable: T | None, error_detail: str) -> T:
     if singleton_variable is None:
-        raise RuntimeError(f"{name} runtime error. Check lifespan.py for proper initialization")
+        raise RuntimeError(error_detail)
     return cast(T, singleton_variable)
+
+def check_property_runtime(name:str, singleton_variable: T | None) -> T:
+    error_detail = f"{name} runtime error. Check lifespan.py for proper initialization"
+    return custom_check_runtime(singleton_variable=singleton_variable, error_detail=error_detail)
