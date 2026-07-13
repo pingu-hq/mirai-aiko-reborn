fce57e675d6c34b2613e5f8d6d9cbd61e515daad
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-13 23:40:50 +0800

    Refactor: From function to OOP concept.

--- app/core/agents/agent_loader.py
+++ app/core/agents/agent_loader.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import Literal
+
 from crewai import Agent, Task
 from app.core.exceptions import custom_check_runtime
 import yaml
@@ -7,42 +9,63 @@ agent_config_yaml: dict | None = None
 task_config_yaml: dict | None = None
 
 
-def init_yaml_config_files():
-    global agent_config_yaml, task_config_yaml
-    if agent_config_yaml is None:
-        agent_config_yaml = load_yaml_config("agents.yaml")
-    if task_config_yaml is None:
-        task_config_yaml = load_yaml_config("tasks.yaml")
+files_available = Literal[
+    "agents.yaml",
+    "tasks.yaml"
+]
 
 
+class AgentLoader:
+    def __init__(self):
+        self._agents = []
+        self._tasks = []
 
+    def init_config(self):
+        global agent_config_yaml, task_config_yaml
+        if agent_config_yaml is None:
+            agent_config_yaml = self.load_yaml("agents.yaml")
+        if task_config_yaml is None:
+            task_config_yaml = self.load_yaml("tasks.yaml")
 
+    @staticmethod
+    def runtime_checker(variable_to_check):
+        return custom_check_runtime(variable_to_check, "Yaml config file not initialized")
 
-def yaml_exception(obj, name_of_file: str, key: str):
-    return custom_check_runtime(
-        singleton_variable=obj,
-        error_detail=f"The {name_of_file} not defined in {key}",
-    )
+    @staticmethod
+    def load_yaml(filename: files_available) -> dict:
+        config_path = Path(__file__).parent / 'config' / filename
+        with open(config_path, 'r') as f:
+            return yaml.safe_load(f)
 
-def load_yaml_config(filename: str) -> dict:
-    config_path = Path(__file__).parent / 'config' / filename
-    with open(config_path, 'r') as f:
-        return yaml.safe_load(f)
+    def load_agent_config(self, agent_name: str) -> dict:
+        checked_config = self.runtime_checker(variable_to_check=agent_config_yaml)
+        return checked_config[agent_name]
 
-def create_agent_from_config(agent_key: str, **overrides ) -> Agent:
-    config_yaml = yaml_exception(
-        obj=agent_config_yaml,
-        name_of_file='agent_config.yaml',
-        key=agent_key
-    )
-    config = config_yaml[agent_key]
-    return Agent(**config, **overrides)
+    def load_task_config(self, task_name: str) -> dict:
+        checked_config = self.runtime_checker(variable_to_check=task_config_yaml)
+        return checked_config[task_name]
 
-def create_task_from_config(task_key: str, **overrides ) -> Task:
-    config_yaml = yaml_exception(
-        obj=task_config_yaml,
-        name_of_file='task_config.yaml',
-        key=task_key
-    )
-    config = config_yaml[task_key]
-    return Task(**config, **overrides)
+    def create_agent(self, agent_name: str, **overrides) -> Agent:
+        config_file = self.load_agent_config(agent_name=agent_name)
+        new_agent = Agent(**config_file, **overrides)
+        self._agents.append(new_agent)
+        return new_agent
+
+    def create_task(self, task_name: str, agent_assigned: Agent, **overrides) -> Task:
+        config_file = self.load_task_config(task_name=task_name)
+        new_task = Task(**config_file, agent=agent_assigned, **overrides)
+        self._tasks.append(new_task)
+        return new_task
+
+    def load_agents_and_tasks(self) -> tuple[list[Agent], list[Task]]:
+        for agent in self._agents:
+            if not isinstance(agent, Agent) or self._agents is []:
+                raise RuntimeError("Agent is not an Agent")
+        for task in self._tasks:
+            if not isinstance(task, Task) or self._tasks is []:
+                raise RuntimeError("Task is not a Task")
+
+        return self._agents, self._tasks
+
+def init_yaml_loader():
+    AgentLoader().init_config()
