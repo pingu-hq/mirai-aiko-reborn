766ef6a2991848cf7ef965eb6afb699cd9a1f48e
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-16 13:35:36 +0800

    Chore: Modify some function, its confusing in the actual code, but this is much abstracted and better as we don't need to keep passing the object to another parameter. Just writing the name of agent it wont need to manually pass the object. Will still keep original as i might use it for reference.

--- app/core/agents/agent_loader.py
+++ app/core/agents/agent_loader.py
@@ -23,8 +23,40 @@ def init_yaml_loader():
         load_yaml(filename=file)
 
 
-
 class AgentLoader:
+    def __init__(self):
+        self._agents: list[Agent] = []
+        self._tasks: list[Task] = []
+        self.agents_dict: dict[str, Agent] = {}
+
+    @property
+    def agents_yaml(self):
+        return load_yaml(filename="agents.yaml")
+
+    @property
+    def tasks_yaml(self):
+        return load_yaml(filename="tasks.yaml")
+
+
+    def create_agent(self, agent_name: str, **overrides) -> Agent:
+        config_file = self.agents_yaml[agent_name]
+        new_agent = Agent(**config_file, **overrides)
+        self.agents_dict[agent_name] = new_agent
+        self._agents.append(new_agent)
+        return new_agent
+
+    def create_task(self, task_name: str, agent_assigned: str, **overrides) -> Task:
+        config_file = self.tasks_yaml[task_name]
+        agent = self.agents_dict[agent_assigned]
+        new_task = Task(**config_file, agent=agent, **overrides)
+        self._tasks.append(new_task)
+        return new_task
+
+    def agents_and_tasks_as_params(self) -> dict[str, list[Agent] | list[Task]]:
+        return {"agents": self._agents, "tasks": self._tasks}
+
+
+class AgentLoaderVersion1:
     def __init__(self):
         self._agents: list[Agent] = []
         self._tasks: list[Task] = []
