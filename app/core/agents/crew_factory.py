45c22832c1ff41c4b12e1858a0bf34e5d135cce6
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-17 18:37:01 +0800

    Chore: Instead of Dependency injections, since this is not service or repository layer, i think there is no need to depend as i think agent loader can be the source of truth. And no other agent loader can be added. One crew instance is one agent instance, means that its all the same, just the structure.

--- app/core/agents/crew_factory.py
+++ app/core/agents/crew_factory.py
@@ -2,7 +2,6 @@ from crewai import Crew, Process, LLM
 from functools import lru_cache
 from app.core.agents.agent_loader import AgentLoader
 from app.core.local_config import settings
-import os
 
 
 
@@ -25,28 +24,42 @@ def large_llm():
 
 
 class CrewFactory:
-    def __init__(self, agent_loader: AgentLoader):
-        self._agent_loader = agent_loader
+    def __init__(self):
+        self._agent_loader = None
 
-    def create_crew(self) -> Crew:
-        params = self._agent_loader.agents_and_tasks_as_params()
+    @property
+    def agent_loader(self) -> AgentLoader:
+        if self._agent_loader is None:
+            self._agent_loader = AgentLoader()
+        return self._agent_loader
+
+    def build_crew(self, **kwargs):
+        params = self.agent_loader.agents_and_tasks_as_params()
         return Crew(
             **params,
             process=Process.sequential,
-            verbose=True
+            verbose=True,
+            **kwargs
         )
+
+    def run(self, inputs: dict[str, str] | None = None,**kwargs) -> str:
+        _crew_instance = self.build_crew(**kwargs)
+        results = _crew_instance.kickoff(inputs=inputs) if inputs else _crew_instance.kickoff()
+        return results.raw
+
+
 
 
 def run_mirai_aiko():
-    agent_loader = AgentLoader()
-    agent_loader.create_agent("researcher", llm=small_llm())
-    agent_loader.create_agent("writer", llm=small_llm())
-    agent_loader.create_task("research_task", "researcher")
-    agent_loader.create_task("writing_task", "writer")
-    crew_instance = CrewFactory(agent_loader)
-    the_crew = crew_instance.create_crew()
-    result = the_crew.kickoff()
-    print(result.raw)
+    factory = CrewFactory()
+
+    factory.agent_loader.create_agent("researcher", llm=small_llm())
+    factory.agent_loader.create_agent("writer", llm=small_llm())
+    factory.agent_loader.create_task("research_task", "researcher")
+    factory.agent_loader.create_task("writing_task", "writer")
+    result = factory.run()
+    print(result)
+    return result
 
 
-run_mirai_aiko()
+# run_mirai_aiko()
