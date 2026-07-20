4462ba43ff27ef406687f282148ae36b49a4e7d0
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-20 21:36:31 +0800

    Refactor/Chore: - Migrated from JWT auth service to Opaque Token auth service

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, status, HTTPException, Depends
-from app.services.auth.web_auth_service import HttpCookieManagerService
+from app.services.auth.opaque_auth_service import OpaqueAuthService
 from app.dependencies.auth import (
-    get_http_cookie_manager_service,
     get_user_login_service,
     get_user_create_service,
-    get_user_id_from_cookie,
-    get_login_state_service
+    get_login_state_service,
+get_opaque_auth_service
 )
 from app.core.logger import app_logger
 from app.services.auth.user_auth_services import AuthUserLoginService, AuthUserRegisterService, LoginStateService
 from app.schemas.users import *
+
 
 router = APIRouter(
     prefix="/api/auth",
@@ -21,9 +21,9 @@ router = APIRouter(
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_endpoint_v1(
         user: UserLoginV1,
-        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
+        state: LoginStateService = Depends(get_login_state_service),
+        opaque_token: OpaqueAuthService = Depends(get_opaque_auth_service),
         login_service: AuthUserLoginService = Depends(get_user_login_service),
-        state: LoginStateService = Depends(get_login_state_service)
 ):
     try:
         sub_id = await login_service.get_id_for_token_from_login_v1(user=user)
@@ -34,7 +34,7 @@ async def login_endpoint_v1(
         if not await state.login_by_id(input_id=sub_id):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized Error")
 
-        http_cookie.setting_http_cookie(sub=sub_id)
+        await opaque_token.login_user(sub_id)
         app_logger.info(f"The User ({user.email}) has successfully logged in")
         return {"status": "Ok", "extra": user.email}
     except Exception as e:
@@ -59,10 +59,13 @@ async def signup_endpoint_v1(
 
 
 @router.get("/me")
-async def check_user_v1(user_id: str = Depends(get_user_id_from_cookie)):
+async def check_user_v1(opaque_token: OpaqueAuthService = Depends(get_opaque_auth_service)):
     try:
+        user_id = await opaque_token.get_user_id()
+        access = opaque_token.get_raw_token_from_http_cookie("access")
+        refresh = opaque_token.get_raw_token_from_http_cookie("refresh")
         app_logger.info(f"The User ({user_id}) exists in the cookie")
-        return {"test_user_id": user_id}
+        return {"test_user_id": user_id, "extra": {"access":access, "refresh": refresh}}
     except HTTPException:
         raise
     except Exception as e:
@@ -72,10 +75,10 @@ async def check_user_v1(user_id: str = Depends(get_user_id_from_cookie)):
 
 @router.delete("/logout", status_code=status.HTTP_204_NO_CONTENT)
 async def logout_endpoint_v1(
-        http_cookie: HttpCookieManagerService = Depends(get_http_cookie_manager_service),
-        user_id: str = Depends(get_user_id_from_cookie),
+        opaque_token: OpaqueAuthService = Depends(get_opaque_auth_service),
         state: LoginStateService = Depends(get_login_state_service)
 ):
-    http_cookie.deleting_id_from_http_cookie()
+    user_id = await opaque_token.get_user_id()
+    await opaque_token.logout_user()
     await state.logout_by_id(input_id=user_id)
     app_logger.info("User logged out")
