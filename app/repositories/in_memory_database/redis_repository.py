a5bcccb6316b543bc2c5854350dc0b53faf3428a
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-20 21:24:20 +0800

    Feature: - Adding an async Redis client - Other redis is synchronous to make up with the jwt cryptography and algorith to encode and decode in blocking manner - Async redis can be reused by fastapi endpoints as im going for full async code. - Has init and close for lifespan.py

--- app/repositories/in_memory_database/redis_repository.py
+++ app/repositories/in_memory_database/redis_repository.py
@@ -1,5 +1,9 @@
+import json
 from datetime import timedelta
-from redis import Redis
+from typing import Any
+
+from redis import Redis, RedisError
+from redis.asyncio.client import Redis as RedisAsync
 from app.core.exceptions import check_property_runtime
 
 
@@ -41,3 +45,63 @@ class JtiCacheRepository:
 
     def delete(self, name):
         self.client.delete(name)
+
+
+
+_redis_client_async: RedisAsync | None = None
+
+def init_redis_client_async():
+    global _redis_client_async
+    if _redis_client_async is None:
+        _redis_client_async = RedisAsync(
+            host='127.0.0.1',
+            port=6379,
+            db=0,
+            decode_responses=True
+        )
+
+async def close_redis_client_async():
+    global _redis_client_async
+    if _redis_client_async:
+        await _redis_client_async.close()
+
+class RedisAsyncRepository:
+
+    @property
+    def redis_client(self) -> RedisAsync:
+        return _redis_client_async
+
+
+    async def add_value(self, input_key: str, input_value: str | dict[str, Any] | bytes, exp: timedelta):
+        try:
+            if isinstance(input_value, dict):
+                input_value = json.dumps(input_value)
+            await self.redis_client.set(
+                name=input_key,
+                value=input_value,
+                ex=exp
+            )
+            return True
+        except RedisError as e:
+            raise RedisError(e)
+        except Exception as e:
+            raise e
+
+    async def get_value(self, input_key: str):
+        try:
+            raw_output_string = await self.redis_client.get(name=input_key)
+
+            if raw_output_string is not None:
+                return json.loads(raw_output_string)
+
+            return None
+        except json.JSONDecodeError as e:
+            raise RedisError(e)
+        except Exception as e:
+            raise e
+
+    async def delete_value(self, input_key):
+        if input_key is not None:
+            await self.redis_client.delete(input_key)
+            return True
+        return False
