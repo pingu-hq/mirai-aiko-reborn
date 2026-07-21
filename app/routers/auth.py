248985098a325699eea1a889165875ed76cf67cb
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-21 18:08:36 +0800

    Refactor: - Refactoring login endpoint - Removed login state service - Uses the new opaque token service to extract token from cookies and put tokens to cookies - Add additional error raises to ensure we catch unexpected error - Added login block from opaque token service, to block re-login user but allow first time or those logged out to their devices. - Remove login state service from logout endpoint, only in login we need that. - Redis will handle the state for user logging in to their device or per device.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from app.services.auth.opaque_auth_service import OpaqueAuthService
 from app.dependencies.auth import (
-    get_user_login_service,
-    get_user_create_service,
-    get_login_state_service,
+get_user_login_service,
+get_user_create_service,
 get_opaque_auth_service
 )
 from app.core.logger import app_logger
-from app.services.auth.user_auth_services import AuthUserLoginService, AuthUserRegisterService, LoginStateService
+from app.services.auth.user_auth_services import AuthUserLoginService, AuthUserRegisterService
 from app.schemas.users import *
 
 
@@ -20,26 +19,26 @@ router = APIRouter(
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_endpoint_v1(
-        user: UserLoginV1,
-        state: LoginStateService = Depends(get_login_state_service),
+        user_login: UserLoginV1,
         opaque_token: OpaqueAuthService = Depends(get_opaque_auth_service),
         login_service: AuthUserLoginService = Depends(get_user_login_service),
 ):
     try:
-        sub_id = await login_service.get_id_for_token_from_login_v1(user=user)
-        if not sub_id:
-            app_logger.error(f"The User ({user.email}) password is incorrect")
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized Error")
+        sub_id = await login_service.login_user_to_get_id(user=user_login)
 
-        if not await state.login_by_id(input_id=sub_id):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized Error")
+        await opaque_token.block_re_login_user(sub_id)
 
         await opaque_token.login_user(sub_id)
-        app_logger.info(f"The User ({user.email}) has successfully logged in")
-        return {"status": "Ok", "extra": user.email}
+
+        app_logger.info(f"The User ({user_login.email}) has successfully logged in")
+        return {"status": "Ok", "extra": user_login.email}
+
+    except HTTPException:
+        raise
+
     except Exception as e:
-        app_logger.error(f"User login error: {e}")
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized Error")
+        app_logger.error(f"User login error: {str(e)}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal error server")
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
@@ -76,9 +75,6 @@ async def check_user_v1(opaque_token: OpaqueAuthService = Depends(get_opaque_aut
 @router.delete("/logout", status_code=status.HTTP_204_NO_CONTENT)
 async def logout_endpoint_v1(
         opaque_token: OpaqueAuthService = Depends(get_opaque_auth_service),
-        state: LoginStateService = Depends(get_login_state_service)
 ):
-    user_id = await opaque_token.get_user_id()
     await opaque_token.logout_user()
-    await state.logout_by_id(input_id=user_id)
     app_logger.info("User logged out")
