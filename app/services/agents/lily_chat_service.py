f260e6438b8d03c5904db0dfb0b856822198f43d
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-29 14:13:05 +0800

    Chore: - Remove mem0 and groq in initializer as i would use _tools instead. - gpt_oss_120 will be removed from service and be put inside tools - mem0 search and add memory will be removed from service and be put inside tools - for DRY, i made a groq completion base and be used for chat completion and web search - change func name from "run" to "execute_task"

--- app/services/agents/lily_chat_service.py
+++ app/services/agents/lily_chat_service.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Literal
+from typing import Literal, Any
 from app.services.agents.llm_clients import get_groq_async_client
 from app.services.data.memory_service import AsyncMemZeroMemoryService
 from jinja2 import Template
@@ -31,8 +31,51 @@ class LilyToolsService:
         self.mem = AsyncMemZeroMemoryService()
         self.groq_client = get_groq_async_client()
 
+    async def groq_base_chat_completion(
+            self,
+            model: str,
+            messages: list[dict[str, Any]],
+            temp: float,
+            tokens: int,
+            **kwargs
+    ):
+        return await self.groq_client.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=temp,
+            max_completion_tokens=tokens,
+            top_p=1,
+            **kwargs
+        )
+
+
+    async def gpt_oss_120b(
+            self,
+            system_content: str,
+            user_content: str,
+            reasoning_effort: Literal["none", "low", "medium", "high"] = "low"
+    ) -> str:
+        completion = await self.groq_base_chat_completion(
+            model="openai/gpt-oss-120b",
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_content
+                },
+                {
+                    "role": "user",
+                    "content": user_content
+                }
+            ],
+            tokens=10_000,
+            temp=.2,
+            reasoning_effort=reasoning_effort,
+            response_format={"type": "json_object"},
+        )
+        return completion.choices[0].message.content
+
     async def web_search(self, user_input: str) -> str:
-        completion = await self.groq_client.chat.completions.create(
+        completion = await self.groq_base_chat_completion(
             model="groq/compound-mini",
             messages=[
                 {
@@ -40,10 +83,8 @@ class LilyToolsService:
                     "content": user_input
                 }
             ],
-            temperature=1,
-            max_completion_tokens=8192,
-            top_p=1,
-            stop=None,
+            temp=.5,
+            tokens=8192,
             compound_custom={
                 "tools": {
                     "enabled_tools": [
@@ -61,12 +102,17 @@ class LilyToolsService:
     async def add_memory(self, user_input: str, user_id: str):
         await self.mem.add_memory(user_id=user_id, content=user_input)
 
+    async def search_memory_context(self, user_id: str, user_input: str):
+        return await self.mem.raw_search_memory(
+            user_id=user_id,
+            query=user_input,
+            explain=True,
+            output="raw"
+        )
 
 class LilyChatRouterService:
     def __init__(self, tools: LilyToolsService):
         self._tools = tools
-        self.mem = AsyncMemZeroMemoryService()
-        self.groq_client = get_groq_async_client()
 
     @property
     def ttl_cache(self) -> TTLCache:
@@ -111,61 +157,28 @@ class LilyChatRouterService:
     def get_messages_cache(self, user_id: str):
         return list(self.ttl_cache.get(user_id, []))
 
-    async def gpt_oss_120b(
+
+    async def execute_tasks(
             self,
-            system_content: str,
-            user_content: str,
-            reasoning_effort: Literal["none", "low", "medium", "high"] = "low"
-    ) -> str:
-        completion = await self.groq_client.chat.completions.create(
-            model="openai/gpt-oss-120b",
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_content
-                },
-                {
-                    "role": "user",
-                    "content": user_content
-                }
-            ],
-            temperature=.2,
-            max_completion_tokens=10_000,
-            top_p=1,
-            reasoning_effort=reasoning_effort,
-            response_format={"type": "json_object"},
-        )
-        return completion.choices[0].message.content
+            user_id: str,
+            user_input: str,
+            recent_conversation: list,
+        ):
+            memory_context = await self._tools.search_memory_context(
+                user_id=user_id,
+                user_input=user_input
+            )
+            user_content = await self.finalize_user_content(
+                user_input=user_input,
+                memory_context=memory_context,
+                recent_conversation=recent_conversation
+            )
+            app_logger.debug(f"Finalized user content: {user_content}")
+            system_prompt = ChatPromptLoader.get_prompts(phase=PHASE_1, prompt=LILY_SYSTEM_PROMPT)
 
-    async def run(self, user_id: str, user_input: str):
-        memory_context = await self.mem.raw_search_memory(
-            user_id=user_id,
-            query=user_input,
-            explain=True,
-            output="raw"
-        )
-        app_logger.debug(f"Internal memory_context: {memory_context}")
-        recent_conversation = self.get_messages_cache(user_id=user_id)
-        app_logger.debug(f"Internal recent conversation: {recent_conversation}")
-        user_content = await self.finalize_user_content(
-            user_input=user_input,
-            memory_context=memory_context,
-            recent_conversation=recent_conversation
-        )
-        app_logger.debug(f"Finalized user content: {user_content}")
-        system_prompt = ChatPromptLoader.get_prompts(phase=PHASE_1, prompt=LILY_SYSTEM_PROMPT)
-        response = await self.gpt_oss_120b(
-            system_content=system_prompt,
-            user_content=user_content
-        )
-        app_logger.debug(f"Chat completion response: {response}")
-        return response
-
-        app_logger.debug(f"Finalized user content: {user_content}")
-
-        response = await self.gpt_oss_120b(
-            system_content=SYSTEM_PROMPT,
-            user_content=user_content
-        )
-        app_logger.debug(f"Chat completion response: {response}")
-        return response
+            response = await self._tools.gpt_oss_120b(
+                system_content=system_prompt,
+                user_content=user_content
+            )
+            app_logger.debug(f"Chat completion response: {response}")
+            return response
