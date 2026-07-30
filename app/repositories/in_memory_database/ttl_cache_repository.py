754047a1e2cd0d03935ca50694a1e02e97cc0bd1
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-30 16:03:44 +0800

    Feature: - Added Test repository, this seems much cleaner. but im still checking and thinking if i should use this kind of pattern. - This is for testing purpose, but if worth it, will transfer to this class for chat message cache

--- app/repositories/in_memory_database/ttl_cache_repository.py
+++ app/repositories/in_memory_database/ttl_cache_repository.py
@@ -52,3 +52,51 @@ class ChatMessageCacheRepository:
 
 
 
+
+
+
+class ChatMessageCacheTestRepository:
+    _ttl_cache: TTLCache | None = None
+    _user_locks: TTLCache | None = None
+
+    @classmethod
+    def init_chat_messages_cache(cls):
+        if cls._ttl_cache is None:
+            cls._ttl_cache = TTLCache(
+                maxsize=1000,
+                ttl=timedelta(hours=10).total_seconds(),
+            )
+
+        if cls._user_locks is None:
+            cls._user_locks = TTLCache(
+                maxsize=1000,
+                ttl=timedelta(hours=10).total_seconds(),
+            )
+
+    @classmethod
+    def close_chat_messages_cache(cls):
+        if cls._ttl_cache:
+            cls._ttl_cache.clear()
+
+        if cls._user_locks:
+            cls._user_locks.clear()
+
+    @property
+    def message_cache(self) -> TTLCache:
+        return self._ttl_cache
+
+    @property
+    def user_locks(self) -> TTLCache:
+        return self._user_locks
+
+    def get_user_lock(self, user_id: str) -> Lock:
+        return self.user_locks.setdefault(user_id, Lock())
+
+    async def get_messages(self, user_id: str):
+        async with self.get_user_lock(user_id):
+            return self.message_cache.setdefault(user_id, [])
+
+    async def append_message(self, user_id: str, message):
+        async with self.get_user_lock(user_id):
+            messages = self.message_cache.setdefault(user_id, [])
+            messages.append(message)
