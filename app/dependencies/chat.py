f00cfa555b5b12ef0d4de26d0b4c2bca9819b946
Nana Mein <alfeoephraimquintay@gmail.com>
2026-08-03 00:53:44 +0800

    Chore: - Disable lotus crew service, as i dont have any task for it to do. - Only lily works fine for now

--- app/dependencies/chat.py
+++ app/dependencies/chat.py
@@ -1,6 +1,6 @@
 from app.services.data.message_vector_services import MessageVectorService
 from app.repositories.no_sql_database.milvus_vector_repository import MessageStoreRepository
-from app.services.agents.lotus_crew_service import LotusCrewService
+# from app.services.agents.lotus_crew_service import LotusCrewService
 from app.services.data.memory_service import MemZeroMemoryService, AsyncMemZeroMemoryService
 from fastapi import Depends
 
@@ -17,8 +17,8 @@ def get_message_vector_service(
         message_repository=message_repository
     )
 
-def get_lotus_crew_service():
-    return LotusCrewService()
+# def get_lotus_crew_service():
+#     return LotusCrewService()
 
 def get_mem_zero_service():
     return MemZeroMemoryService()
