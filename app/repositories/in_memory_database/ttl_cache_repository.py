8fa3c01890d577278e41b195280674b027e9d49a
Nana Mein <alfeoephraimquintay@gmail.com>
2026-08-03 15:40:10 +0800

    Refactor: - refactor from using one master lock and ttl cache to using 2 ttl cache without - one ttl cache still holds user id and messages inside - the other ttl cache holds only lock per user id - using these 2 locks, be able to safely write and read, without mutation (in theory) - get messages and append messages are 2 functions can be used.

--- app/repositories/in_memory_database/ttl_cache_repository.py
+++ app/repositories/in_memory_database/ttl_cache_repository.py
@@ -4,51 +4,52 @@ from asyncio import Lock
 
 
 class ChatMessageCacheRepository:
-    _ttl_cache: TTLCache | None = None
-    _lock: Lock | None = None
+    _user_message_cache: TTLCache | None = None
+    _user_locks: TTLCache | None = None
+    # _lock: Lock | None = None
 
     @classmethod
     def init_chat_messages_cache(cls):
-        if cls._ttl_cache is None:
-            cls._ttl_cache = TTLCache(
+        if cls._user_message_cache is None:
+            cls._user_message_cache = TTLCache(
+                maxsize=1000,
+                ttl=timedelta(hours=10).total_seconds()
+            )
+        if cls._user_locks is None:
+            cls._user_locks = TTLCache(
                 maxsize=1000,
                 ttl=timedelta(hours=10).total_seconds()
             )
 
     @classmethod
     def close_chat_messages_cache(cls):
-        if cls._ttl_cache:
-            cls._ttl_cache.clear()
+        if cls._user_message_cache:
+            cls._user_message_cache.clear()
 
-    @classmethod
-    def get_master_lock(cls) -> Lock:
-        if cls._lock is None:
-            cls._lock = Lock()
-        return cls._lock
-
+        if cls._user_locks:
+            cls._user_locks.clear()
 
     @property
     def message_cache(self) -> TTLCache:
-        return self._ttl_cache
+        return self._user_message_cache
 
+    @property
+    def user_locks(self) -> TTLCache:
+        return self._user_locks
 
-    async def _add_value(self, user_id: str, value):
-        async with self.get_master_lock():
-            self.message_cache[user_id] = value
+    def get_user_lock(self, user_id: str) -> Lock:
+        return self.user_locks.setdefault(user_id, Lock())
 
-    async def _get_value(self, user_id: str):
-        if user_id in self.message_cache:
-            return self.message_cache[user_id]
-
-        async with self.get_master_lock():
-            new_value = []
-            self.message_cache[user_id] = new_value
-            return new_value
-
-    async def append_messages(self, user_id: str, message):
-        async with self.get_master_lock():
+    async def append_message(self, user_id: str, message):
+        async with self.get_user_lock(user_id=user_id):
             current_messages = self.message_cache.setdefault(user_id, [])
             current_messages.append(message)
+
+    async def get_messages(self, user_id: str):
+        async with self.get_user_lock(user_id=user_id):
+            return self.message_cache.setdefault(user_id, [])
+
+
 
 
 
