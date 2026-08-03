41bd0890a08cbe3a25a6fc37a8444499d939d14c
Nana Mein <alfeoephraimquintay@gmail.com>
2026-08-03 15:08:52 +0800

    Chore: - Added new Agent loader as DI for separate testing - Added new crew factory class to be used as testing.

--- app/core/agents/crew_factory.py
+++ app/core/agents/crew_factory.py
@@ -1,9 +1,22 @@
 from typing import Any, Literal
-from crewai import Crew, Process, LLM
+from crewai import Crew, Process, LLM, Flow
+from crewai.crew import CrewStreamingOutput, CrewOutput
 from cachetools import TTLCache
-from app.core.agents.agent_loader import AgentLoader
+from app.core.agents.agent_loader import AgentLoader,SampleAgentLoader
 from app.core.local_config import settings
 from threading import Lock
+import os
+
+
+# Disables CrewAI from phoning home with anonymous usage data
+os.environ["CREWAI_DISABLE_TELEMETRY"] = "true"
+
+# Disables the annoying 20-second terminal prompt asking to view traces
+os.environ["CREWAI_TRACING_ENABLED"] = "false"
+
+# (Optional) The nuclear option: disables ALL OpenTelemetry globally
+# Only use this if you aren't using OpenTelemetry for anything else!
+os.environ["OTEL_SDK_DISABLED"] = "true"
 
 
 
@@ -52,12 +65,19 @@ def close_cache_and_crew_llms():
 class CrewFactory:
     def __init__(self):
         self._agent_loader = None
+        self._loader = None
 
     @property
     def agent_loader(self) -> AgentLoader:
         if self._agent_loader is None:
             self._agent_loader = AgentLoader()
         return self._agent_loader
+
+    @property
+    def loader(self) -> SampleAgentLoader:
+        if self._loader is None:
+            self._loader = SampleAgentLoader()
+        return self._loader
 
     def build_crew(self, **kwargs):
         params = self.agent_loader.agents_and_tasks_as_params()
@@ -107,18 +127,62 @@ class CrewFactory:
         return results.raw
 
 
+class CrewFactoryVersion1:
+    def __init__(self):
+        self._loader = None
 
+    @property
+    def loader(self) -> SampleAgentLoader:
+        if self._loader is None:
+            self._loader = SampleAgentLoader()
+        return self._loader
 
-def run_mirai_aiko():
-    factory = CrewFactory()
+    def build_crew(self, **kwargs):
+        kwargs.setdefault("verbose", True)
+        kwargs.setdefault("process", Process.sequential)
+        params = self.loader.agents_and_tasks_as_params()
+        return Crew(
+            **params,
+            **kwargs
+        )
 
-    factory.agent_loader.create_agent("researcher", llm=small_llm())
-    factory.agent_loader.create_agent("writer", llm=small_llm())
-    factory.agent_loader.create_task("research_task", "researcher")
-    factory.agent_loader.create_task("writing_task", "writer")
-    result = factory.run()
-    print(result)
-    return result
+    def create_agent_llm(
+            self,
+            model: Literal["small","big"],
+            reasoning_effort: Literal["none", "low", "medium", "high"] | None = None,
+            max_completion_tokens: int = 10_000,
+            temperature: float = .5,
 
+    ):
+        global _LLM_HOLDER
+        if model == "small":
+            gpt_oss_model = "groq/openai/gpt-oss-20b"
+        else:
+            gpt_oss_model = "groq/openai/gpt-oss-120b"
 
-# run_mirai_aiko()
+        llm_params = {
+            "base_url":"https://api.groq.com/openai/v1",
+            "api_key":settings.groq_api_key.get_secret_value(),
+            "top_p":1,
+        }
+
+        cache_key = f"{model}:{reasoning_effort}:{max_completion_tokens}"
+        with _cache_lock:
+            if cache_key not in _llm_cache:
+                _llm_cache[cache_key] = LLM(
+                    model=gpt_oss_model,
+                    reasoning_effort=reasoning_effort,
+                    max_completion_tokens=max_completion_tokens,
+                    temperature=temperature,
+                    **llm_params
+                )
+            return _llm_cache[cache_key]
+
+    def run(self, inputs: dict[str, Any] | None = None,**kwargs) -> str:
+        _crew_instance = self.build_crew(**kwargs)
+        results = _crew_instance.kickoff(inputs=inputs) if inputs else _crew_instance.kickoff()
+        return results.raw
+
+    async def run_async(self, inputs: dict[str, Any] | None = None, **kwargs) -> CrewOutput | CrewStreamingOutput:
+        _crew_instance = self.build_crew(**kwargs)
+        return await _crew_instance.kickoff_async(inputs=inputs)
