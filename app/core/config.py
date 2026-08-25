ac9bb44c83c3b24a85da310d04959b39f8e02017
Nana Mein <alfeoephraimquintay@gmail.com>
2026-08-25 22:48:19 +0800

    Feat: - Added Redis and Mongo Uri

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 class Settings:
     REDIS_URL: str | None = os.getenv("REDIS_URL", None)
+    MONGO_URI: str | None = os.getenv("MONGO_URL", None)
+    REDIS_URI: str | None = os.getenv("REDIS_URI", None)
     PH_TZ: ZoneInfo = ZoneInfo("Asia/Manila")
     IS_DEPLOYED_FOR_PRODUCTION: bool = os.getenv("IS_DEPLOYED_FOR_PRODUCTION", "false").lower() == "true"
 
@@ -26,6 +28,19 @@ class Settings:
     @property
     def is_deployed_for_production(self) -> bool:
         return self.IS_DEPLOYED_FOR_PRODUCTION
+
+
+    @property
+    def mongo_uri(self) -> str:
+        if not self.MONGO_URI:
+            return "mongodb://mongo:27017/" # from Docker default
+        return self.MONGO_URI
+
+    @property
+    def redis_uri(self) -> str:
+        if not self.REDIS_URI:
+            return "redis://redis:6379/0" # from Docker default
+        return self.REDIS_URI
 
 
 settings = Settings()
