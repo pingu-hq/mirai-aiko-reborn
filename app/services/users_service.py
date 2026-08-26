3f9a180ad17e599eb8eac476c66a24f513ea3aad
Nana Mein <alfeoephraimquintay@gmail.com>
2026-08-26 21:26:11 +0800

    Refactor: - Made big changes, using Annotated and Depends for the class. - Added http cookie service to abstract and built in validation so that   user only needs to login in and these services will manage cookie,   redis for opaque and error rotate keys when tokens are expired. - Remove user schema in class parameter but include in a function to be   inserted and be used internally.

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -1,27 +1,44 @@
 from datetime import datetime
+from typing import Annotated
 
-from fastapi import HTTPException, status
+from fastapi import Depends, HTTPException, status
 from pymongo.asynchronous.collection import AsyncCollection
 from pymongo.errors import DuplicateKeyError
 
 from app.core.config import settings
 from app.repositories.database import UsersRepository
 from app.schemas.users_schema import UserLoginSchema, UserRegisterSchema
+from app.services.http_cookie_service import (
+    HttpCookieAuthService,
+    get_http_cookie_auth_service,
+)
 from app.services.password_hasher_service import PasswordHasherService
 
 
 class UserLoginService:
     def __init__(
         self,
-        user_schema: UserLoginSchema,
+        http_cookie_service: HttpCookieAuthService,
         user_repo: UsersRepository | None = None,
         password_hasher: PasswordHasherService | None = None,
     ):
         self.user_repo = user_repo or UsersRepository()
         self.password_hasher = password_hasher or PasswordHasherService()
+        self.user_schema: UserLoginSchema | None = None
+        self.http_cookie_service = http_cookie_service
+
+    def insert_user_schema(self, user_schema: UserLoginSchema):
         self.user_schema = user_schema
+        return self
+
+    
 
     async def _get_user(self):
+        if self.user_schema is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Bad Request",
+            )
         users_collection: AsyncCollection = await self.user_repo.get_users_collection()
         user_profile: dict | None = await users_collection.find_one(
             filter={"email": self.user_schema.email}
@@ -37,7 +54,7 @@ class UserLoginService:
             return user_profile.get("username")
         return None
 
-    async def get_user(self) -> str:
+    async def get_validated_username_from_db(self) -> str:
         user = await self._get_user()
         if user is None:
             raise HTTPException(
@@ -46,6 +63,34 @@ class UserLoginService:
             )
         return user
 
+    async def login_user_and_set_cookie(self):
+        validated_user = await self.get_validated_username_from_db()
+        if not validated_user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Unauthorized",
+            )
+        if await self.http_cookie_service.is_user_still_logged_in():
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Bad Request",
+            )
+        await self.http_cookie_service.set_http_cookie(sub=validated_user, token_type="access")
+        await self.http_cookie_service.set_http_cookie(sub=validated_user, token_type="refresh")
+        return True
+
+
+def get_user_login_service(
+    http_cookie_service: Annotated[HttpCookieAuthService, Depends(get_http_cookie_auth_service)]
+) -> UserLoginService:
+    return UserLoginService(http_cookie_service=http_cookie_service)
+
+UserLoginServiceDeps: Annotated[UserLoginService, Depends(get_user_login_service)]
+
+
+
+
+    
 
 class UserRegisterService:
     def __init__(
