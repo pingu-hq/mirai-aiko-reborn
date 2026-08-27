ee9558ad3419ac56f409c385e23f257af47f3106
Nana Mein <alfeoephraimquintay@gmail.com>
2026-08-27 13:03:30 +0800

    Feat: - Added lifespan to initial start and close sockets for redis and   mongodb connections.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.repositories.caches import RedisCacheBaseRepository
+from app.repositories.database import MongoBase
 from app.routers.auth_router import router as auth_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    RedisCacheBaseRepository.init_redis_cache_repository()
+    MongoBase.init_mongo_client()
+    yield
+    await RedisCacheBaseRepository.close_redis_cache_repository()
+    await MongoBase.close_mongo_client()
 
 app = FastAPI(
     version="0.1.0",
     title="Mirai Aiko (Reborn Version)",
+    lifespan=lifespan,
 )
 
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth for users with sample user test"])
